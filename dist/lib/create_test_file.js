aff0a82bd28fe224a5f6a0b6c3a80079f281a2a0
sampsonwang <sampsonwang@tencent.com>
2020-10-12 14:45:08 +0800

    delete dist

--- dist/lib/create_test_file.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class CreateTestFile {
-    static create() {
-        console.log('create on file.');
-    }
-}
-exports.default = CreateTestFile;
